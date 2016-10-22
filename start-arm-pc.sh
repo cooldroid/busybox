69ea50143e3044daac09cc5f4372d10aa99a39fe
CoolDroid <amit.bagaria@gmail.com>
2016-10-22 16:19:33 +0530

    Synced with yashdsaraf patches

--- start-arm-pc.sh
+++ start-arm-pc.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 UCLIBC_HOME="${HOME}/arm-uclibc"
-export PATH=${UCLIBC_HOME}/bin:"$PATH"
-#export LD_LIBRARY_PATH=${UCLIBC_HOME}/lib:"$LD_LIBRARY_PATH"
+SYSROOT=$(cd ${UCLIBC_HOME}/arm-*/sysroot; pwd)
+TOOLCHAIN=`ls ${UCLIBC_HOME}/bin | grep -E "arm.+-linux-.+gcc$" | awk -F-gcc '{print $1}'`
+PATH=${UCLIBC_HOME}/bin:$PATH
+#export LD_LIBRARY_PATH=${UCLIBC_HOME}/${TOOLCHAIN}/sysroot/usr/lib:"$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${SYSROOT}/lib/pkgconfig
+export LD_LIBRARY_PATH=${UCLIBC_HOME}/lib
+
+CMD_PREFIX="ARCH=arm CROSS_COMPILE=${TOOLCHAIN}- CFLAGS=\"-Os -I${UCLIBC_HOME}/include\""
 
 if test ! -f "./.config"; then
-	make cooldroid_selinux_defconfig CROSS_COMPILE=arm-buildroot-linux-uclibcgnueabi-
+	eval make cooldroid_selinux_defconfig
 fi
 
-make -j4 "$@" CROSS_COMPILE=arm-buildroot-linux-uclibcgnueabi-
+sed -i.bak -e "s|.*CONFIG_SYSROOT=.*|CONFIG_SYSROOT=\"$SYSROOT\"|" .config && touch -r .config.bak .config
+eval $CMD_PREFIX make -j4 "$@"
